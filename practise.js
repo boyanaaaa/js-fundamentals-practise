cdb5a92f39545358b597d4b7cd794080d5b0e347
Boyana Dimitrova <boyanadimitrova11@gmail.com>
2024-09-09 17:27:52 +0300

    Guest list task is completed.

--- practise.js
+++ practise.js
@@ -102,7 +102,7 @@ if ( n < 1) {
 
 
 //Launch countdown
-
+/*
 const output = document.querySelector('.output');
 for(let i = 10; i >= 0; i-- ){
     const para = document.createElement('p');
@@ -113,7 +113,34 @@ for(let i = 10; i >= 0; i-- ){
         para.textContent = "Blast off!";
     } else {
         para. textContent = i;
-    }
-
-     
+    }     
 }
+    */
+
+
+
+
+
+
+
+//Guest List
+//In this exercise, we want you to take a list of names stored in an array 
+// and put them into a guest list. But it's not quite that easy — 
+// we don't want to let Phil and Lola in because they are greedy and rude,
+// and always eat all the food! We have two lists, one for guests to admit, and one for guests to refuse.
+/*
+const people = ['Chris', 'Anne', 'Colin', 'Terri', 
+    'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+
+    const admitted = document.getElementsByClassName("admitted")[0];
+    const refused = document.getElementsByClassName("refused")[0];
+    
+    for( const person of people) {
+        if( person === "Phil" || person === "Lola"){
+            refused.textContent  += `${person} `
+        } else {
+            admitted.textContent += `${person} `
+        }
+    }
+*/
+
