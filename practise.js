856df0708c4bdd5ba49ce62102fd51035fad748b
Boyana Dimitrova <boyanadimitrova11@gmail.com>
2024-09-09 16:06:15 +0300

    Countdown exercise completed.

--- practise.js
+++ practise.js
@@ -98,3 +98,22 @@ if ( n < 1) {
 */
 
 
+
+
+
+//Launch countdown
+
+const output = document.querySelector('.output');
+for(let i = 10; i >= 0; i-- ){
+    const para = document.createElement('p');
+    output.appendChild(para);
+    if ( i === 10){
+        para.textContent = " Countdown 10";
+    } else if ( i === 0){
+        para.textContent = "Blast off!";
+    } else {
+        para. textContent = i;
+    }
+
+     
+}
